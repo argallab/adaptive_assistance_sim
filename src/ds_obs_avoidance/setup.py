11f23c34b7f111df7d552a9d20a8ce6457395f38
Deepak Gopinath <musicmagic@gmail.com>
2021-09-05 10:06:35 -0500

    Added all relevant ds_obs_avoidance related modules. To be tested. Restructued folder structure for proper Cmake list. Need to test import into node module.

--- src/ds_obs_avoidance/setup.py
+++ src/ds_obs_avoidance/setup.py
@@ -4,10 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=["ds_systems", "ds_utils", "ds_obstacles", "ds_avoidance", "ds_containers", "ds_avoidance"],
-    package_dir={"": "src"},
-)
+setup_args = generate_distutils_setup(packages=["ds_obs_avoidance"], package_dir={"": "src"},)
 
 
 setup(**setup_args)
---
+++ src/sim_pfields/scripts/sim_ds_obs_node.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Code developed by Deepak Gopinath*, Mahdieh Nejati Javaremi* in February 2020. Copyright (c) 2020. Deepak Gopinath, Mahdieh Nejati Javaremi, Argallab. (*) Equal contribution
+
+import rospy
+import os
+import numpy as np
+import math
+import sys
+import collections
